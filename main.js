9145eef2b57ba2428b36efba4ab1a8475d4c18d0
Nathan Thompson <thompn18@email.franklin.edu>
2024-08-29 21:39:48 -0400

    Implement project sorting

--- main.js
+++ main.js
@@ -1,75 +1,112 @@
 const projects = [
     {
         title: "Title1",
-        description: "Description1",
+        description: "Ut eget facilisis leo, vel finibus risus. Fusce in fringilla \
+                enim. In tempor ut nulla eu posuere. Aliquam non ligula arcu. \
+                Maecenas efficitur nisl magna, non maximus odio malesuada at. In \
+                hac habitasse platea dictumst. Cras at maximus orci.",
         tags: [
-            { name: "tag1", value: "Tag1" },
-            { name: "tag2", value: "Tag2" },
-            { name: "tag3", value: "Tag3" }
+            "Tag1",
+            "Tag2",
+            "Tag3"
         ],
-        difficulty: 1
+        difficulty: 4,
+        startMonth: 1,
+        startYear: 2024,
+        isCompleted: false
     },
     {
         title: "Title2",
         description: "Description2",
         tags: [
-            { name: "tag4", value: "Tag4" },
-            { name: "tag5", value: "Tag5" },
-            { name: "tag6", value: "Tag6" }
+            "Tag4",
+            "Tag5",
+            "Tag6"
         ],
-        difficulty: 1
+        difficulty: 1,
+        startMonth: 2,
+        startYear: 2024,
+        isCompleted: true
     },
     {
         title: "Title3",
-        description: "Description3",
+        description: "Aliquam non ligula arcu. \
+                Maecenas efficitur nisl magna, non maximus odio malesuada at. In \
+                hac habitasse platea dictumst. Cras at maximus orci.",
         tags: [
-            { name: "tag7", value: "Tag7" },
-            { name: "tag8", value: "Tag8" },
-            { name: "tag9", value: "Tag9" }
+            "Tag7",
+            "Tag8",
+            "Tag9"
         ],
-        difficulty: 1
+        difficulty: 4,
+        startMonth: 3,
+        startYear: 2024,
+        isCompleted: false
     },
     {
         title: "Title4",
         description: "Description4",
         tags: [
-            { name: "tag10", value: "Tag10" },
-            { name: "tag11", value: "Tag11" },
-            { name: "tag12", value: "Tag12" }
+            "Tag10",
+            "Tag11",
+            "Tag12"
         ],
-        difficulty: 1
+        difficulty: 1,
+        startMonth: 4,
+        startYear: 2024,
+        isCompleted: false
     },
     {
         title: "Title5",
         description: "Description5",
         tags: [
-            { name: "tag13", value: "Tag13" },
-            { name: "tag14", value: "Tag14" },
-            { name: "tag15", value: "Tag15" }
+            "Tag13",
+            "Tag14",
+            "Tag15"
         ],
-        difficulty: 1
+        difficulty: 2,
+        startMonth: 5,
+        startYear: 2024,
+        isCompleted: true
     },
     {
         title: "Title6",
-        description: "Description6",
+        description: "In tempor ut nulla eu posuere. Aliquam non ligula arcu. \
+                Maecenas efficitur nisl magna, non maximus odio malesuada at. In \
+                hac habitasse platea dictumst. Cras at maximus orci.",
         tags: [
-            { name: "tag16", value: "Tag16" },
-            { name: "tag17", value: "Tag17" },
-            { name: "tag18", value: "Tag18" }
+            "Tag16",
+            "Tag17",
+            "Tag18"
         ],
-        difficulty: 1
+        difficulty: 5,
+        startMonth: 6,
+        startYear: 2024,
+        isCompleted: true
     },
 ];
 
+const sortParameters = [ 
+    "Date",
+    "Title",
+    "Difficulty"
+]
+
+
+
 window.onload = function() {
-    loadProjects();
+    loadSort();
+    loadFilter();
+
+    var projectsToLoad = sortProjectsByStart();
+    loadProjects(projectsToLoad);
 }
 
-function loadProjects() {
+function loadProjects(projectsToLoad) {
     var projectsElement = document.getElementById("projects");
     
-    for (var i = 0; i < projects.length; i++) {
-        var projectElement = createProjectElement(projects[i]);
+    for (var i = 0; i < projectsToLoad.length; i++) {
+        var projectElement = createProjectElement(projectsToLoad[i]);
         projectsElement.append(projectElement);
     }
 }
@@ -83,21 +120,27 @@ function createProjectElement(project) {
 
     var projectDifficultyElement = document.createElement("div");
     projectDifficultyElement.classList.add("project-difficulty");
-    projectDifficultyElement.append("Difficulty: " + project["difficulty"] + "/5");
+    projectDifficultyElement.append("Difficulty: " + project.difficulty + "/5");
 
     var projectTitleElement = document.createElement("div");
     projectTitleElement.classList.add("project-title");
-    projectTitleElement.append(project["title"]);
+    projectTitleElement.append(project.title);
 
     var projectDescriptionElement = document.createElement("div");
     projectDescriptionElement.classList.add("project-description");
-    projectDescriptionElement.append(project["description"]);
+    projectDescriptionElement.append(project.description);
 
-    projectContentElement.append(projectDifficultyElement);
-    projectContentElement.append(projectTitleElement);
-    projectContentElement.append(projectDescriptionElement);
+    var projectStartElement = document.createElement("div");
+    projectStartElement.classList.add("project-start");
+    projectStartElement.append("Started " + getMonthByNumber(project.startMonth) + ", " + project.startYear); 
 
-    projectElement.append(projectContentElement);
+    var projectStatusElement = document.createElement("div");
+    projectStatusElement.classList.add("project-status");
+    if (project.isCompleted) {
+        projectStatusElement.append("Completed"); 
+    } else {
+        projectStatusElement.append("In Progress");
+    }
 
     var projectTagsElement = document.createElement("ul");
     projectTagsElement.classList.add("project-tags");
@@ -105,15 +148,166 @@ function createProjectElement(project) {
     for (var i = 0; i < project.tags.length; i++) {
         var projectTagElement = document.createElement("li");
         projectTagElement.classList.add("project-tag");
-        projectTagElement.append(project.tags[i].value);
+        projectTagElement.append(project.tags[i]);
 
         // Add tag to set of tags for sorting
 
         projectTagsElement.append(projectTagElement);
     }
 
-    projectElement.append(projectTagsElement);
+    projectContentElement.append(projectDifficultyElement);
+    projectContentElement.append(projectTitleElement);
+    projectContentElement.append(projectDescriptionElement);
+    projectContentElement.append(projectStartElement);
+    projectContentElement.append(projectStatusElement);
+    projectContentElement.append(projectTagsElement);
 
+    projectElement.append(projectContentElement);
 
     return projectElement;
+}
+
+function unloadProjects() {
+    var projectsElement = document.getElementById("projects");
+    while (projectsElement.firstChild) {
+        projectsElement.removeChild(projectsElement.firstChild);
+    }
+}
+
+// Sort Members
+
+
+
+function loadSort() {
+    var sortLabelElement = document.createElement("label");
+    sortLabelElement.setAttribute("for", "sortParameters");
+    sortLabelElement.append("Sort");
+
+    var sortSelectElement = document.createElement("select");
+    sortSelectElement.setAttribute("id", "sortParameters");
+
+    for (var i = 0; i < sortParameters.length; i++) {
+        var optionElement = document.createElement("option");
+        optionElement.append(sortParameters[i]);
+        sortSelectElement.append(optionElement);
+    }
+
+    var sortElement = document.getElementById("sort");
+    sortElement.append(sortLabelElement);
+    sortElement.append(sortSelectElement);
+
+    sortSelectElement.onchange = function() {
+
+        var selected = sortSelectElement.value;
+        if (!sortParameters.includes(selected)) {
+            return;
+        }
+
+        unloadProjects();
+        var projectsToLoad = [];
+        switch (selected) {
+            case "Date":
+                projectsToLoad = sortProjectsByStart();
+                break;
+            case "Title":
+                projectsToLoad = sortProjectsByTitle();
+                break;
+            case "Difficulty":
+                projectsToLoad = sortProjectsByDifficulty();
+                break;
+            default:
+                break;
+        }
+
+        loadProjects(projectsToLoad);
+    };
+}
+
+function sortProjectsByStart() {
+    var projectsCopy = [...projects];
+    projectsCopy.sort((a, b) => {
+        if (a.startMonth > b.startMonth) {
+            return 1;
+        }           
+        
+        if (a.startMonth < b.startMonth) {
+            return -1;
+        }
+
+        if (a.startYear > b.startYear) {
+            return -1;
+        }
+
+        if (a.startYear < b.startYear) {
+            return 1;
+        }
+    })
+
+    return projectsCopy;
+}
+
+function sortProjectsByTitle() {
+    var projectsCopy = [...projects];
+    projectsCopy.sort((a, b) => a.title.localeCompare(b.title))
+    return projectsCopy;
+}
+
+function sortProjectsByDifficulty() {
+    var projectsCopy = [...projects];
+    projectsCopy.sort((a, b) => b.difficulty - a.difficulty)
+    return projectsCopy;
+}
+
+// Filter Members
+
+const tags = getUniqueTags();
+
+function loadFilter() {
+
+}
+
+function getUniqueTags() {
+    var result = [];
+    for (var i = 0; i < projects.length; i++) {
+        for (var j = 0; j < projects[i].tags.length; j++) {
+            var tag = projects[i].tags[j];
+            if (!result.includes(tag)) {
+                result.push(tag);
+            }
+        }
+    }
+    return result;
+}
+
+// Utility Members
+
+function getMonthByNumber(number) {
+    switch (number) {
+        case 1:
+            return "Jan";
+        case 2:
+            return "Feb";
+        case 3:
+            return "Mar";
+        case 4:
+            return "Apr";
+        case 5:
+            return "May";
+        case 6:
+            return "Jun";
+        case 7:
+            return "Jul";
+        case 8:
+            return "Aug";
+        case 9:
+            return "Sep";
+        case 10:
+            return "Oct";
+        case 11:
+            return "Nov";
+        case 12:
+            return "Dec";
+        default:
+            break;
+    }
 }
